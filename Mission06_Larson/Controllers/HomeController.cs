e75534d566306c12686e14fef93241c3f05b7269
Davis Larson <davislarson01@gmail.com>
2025-02-19 16:07:38 -0700

    update controller

--- Mission06_Larson/Controllers/HomeController.cs
+++ Mission06_Larson/Controllers/HomeController.cs
@@ -1,5 +1,7 @@
 using System.Diagnostics;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.ModelBinding;
+using Microsoft.EntityFrameworkCore;
 using Mission06_Larson.Models;
 
 namespace Mission06_Larson.Controllers;
@@ -26,17 +28,82 @@ public class HomeController : Controller
     // Movie form route
     public IActionResult MovieFile()
     {
-        return View();
+        ViewBag.Categories = _context.Categories
+            .OrderBy(x => x.CategoryName)
+            .ToList();
+        
+        return View(new Movie());
     }
     
     // Save movie form to the database route
     [HttpPost]
     public IActionResult MovieFile(Movie movie)
     {
-        _context.Movies.Add(movie);
+        if (ModelState.IsValid)
+        {
+            _context.Movies.Add(movie);
+            _context.SaveChanges();
+            
+            return View("Confirmation", movie);
+        }
+        else
+        {
+            ViewBag.Categories = _context.Categories
+                .OrderBy(x => x.CategoryName)
+                .ToList();
+            
+            return View(movie);
+        }
+
+    }
+
+    public IActionResult MovieList()
+    {
+        var movies = _context.Movies
+            .Include(x => x.Category)
+            .OrderBy(x => x.Title)
+            .ToList();
+        
+        return View(movies);
+    }
+
+    [HttpGet]
+    public IActionResult Edit(int filmId)
+    {
+        var record = _context.Movies
+            .Single(x => x.MovieID == filmId);
+        
+        ViewBag.Categories = _context.Categories
+            .OrderBy(x => x.CategoryName)
+            .ToList();
+        
+        return View("MovieFile", record);
+    }
+
+    [HttpPost]
+    public IActionResult Edit(Movie updateRecord)
+    {
+        _context.Update(updateRecord);
+        _context.SaveChanges();
+
+        return RedirectToAction("MovieList");
+    }
+
+    public IActionResult Delete(int filmId)
+    {
+        var recordToDelete = _context.Movies
+            .Single(x => x.MovieID == filmId);
+        
+        return View(recordToDelete);
+    }
+    
+    [HttpPost]
+    public IActionResult Delete(Movie recordToDelete)
+    {
+        _context.Movies.Remove(recordToDelete);
         _context.SaveChanges();
         
-        return View("Confirmation", movie);
+        return RedirectToAction("MovieList");
     }
     
 }
